c4a616aeb15ed0c2297d9a2ec6189c163291e484
unknown <postsynaptic.cell@gmail.com>
2025-03-26 11:47:16 +0300

    add url for clear cart view

--- test-app/app/urls.py
+++ test-app/app/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+add from django.urls import path
 from .views import CategoryListView, ProductListView, ProductDetailView, CartView
-from .views import AddToCartView, UpdateCartItemView, RemoveFromCartView, CustomObtainTokenView
+from .views import AddToCartView, UpdateCartItemView, RemoveFromCartView, ClearCartView, CustomObtainTokenView
 from rest_framework import permissions
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -22,7 +22,8 @@ urlpatterns = [
     path('cart/', CartView.as_view(), name='cart'),
     path('cart/add/', AddToCartView.as_view(), name='add-to-cart'),
     path('cart/remove/<int:item_id>/', RemoveFromCartView.as_view(), name='remove-from-cart'),
-    path('cart/update<int:item_id>/', UpdateCartItemView.as_view(), name='update-cart-item'),
+    path('cart/update/<int:item_id>/', UpdateCartItemView.as_view(), name='update-cart-item'),
+    path('cart/clear/', ClearCartView.as_view(), name='clear-cart'),
     path('api-token-auth', CustomObtainTokenView.as_view(), name='api-token-auth'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
